f47f6379b3310f41c9fc78f2a069b6f33f3c2008
Alef <alefwhite@hotmail.com>
2020-04-28 07:15:03 -0300

    "Crud da MensagemController criado"

--- backend/src/app/controllers/MensagemController.js
+++ backend/src/app/controllers/MensagemController.js
@@ -2,23 +2,83 @@ const connection = require('../../database/connection');
 
 module.exports = {
     async index(req, res) {
-        // const tipoUsuario = await connection('mensagem').select("*");    
+        const mensagens = await connection('mensagem').select("*");    
 
-        // return res.json(mensagem);
+        return res.json(mensagens);
     },
 
     async create(req, res) {        
-        const { nome, email, senha, tipo_usuario } = req.body;
-    
-        const retorno = await connection('mensagem').insert({ 
-            nome,
-            email,
-            senha,
-            tipo_usuario 
+        const { msg } = req.body;
+        const id_usuario = req.headers.authorization;
+
+        let formatter = new Intl.DateTimeFormat('pt-BR', {
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            year: 'numeric',
+            minute: '2-digit'
+        });
+
+        const updated_at = formatter.format(Date.now());
+        const created_at = formatter.format(Date.now());  
+
+        const [retorno] = await connection('mensagem').insert({ 
+            msg,           
+            created_at,
+            updated_at, 
+            id_usuario
         });
        
     
         return res.json({message: "Usuário criado com sucesso!", retorno});
-    }
+    },
+
+    async delete(req, res) {
+        const { id } = req.params;
+        const id_usuario = req.headers.authorization;
+       
+        const mensagem = await connection("mensagem").where('id', id).select("id_usuario").first();
+        const tipoUsuario = await connection("usuario").where('id', id_usuario).select("tipo_usuario").first();
+       
+        if(mensagem.id_usuario == id_usuario || tipoUsuario.tipo_usuario == 1) {
+            await connection('mensagem').where('id', id).delete();
+            return res.json({message: "Mensagem deletada com sucesso!"});
+        }
+        
+        return res.status(401).json({messagem: "Operação não permitida apenas usuário que criou a mensagem ou consultor pode deletar!"})
+       
+    },
+
+    async update(req, res) {
+        const { id } = req.params;
+        const id_usuario = req.headers.authorization;
+        const { msg } = req.body;
+
+        let formatter = new Intl.DateTimeFormat('pt-BR', {
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            year: 'numeric',
+            minute: '2-digit'
+        });
+
+        const updated_at = formatter.format(Date.now());      
+       
+        const mensagem = await connection("mensagem").where('id', id).select("id_usuario").first();
+        const tipoUsuario = await connection("usuario").where('id', id_usuario).select("tipo_usuario").first();
+        
+        if(mensagem.id_usuario == id_usuario || tipoUsuario.tipo_usuario == 1) {
+            await connection('mensagem').where('id', id).update({
+                msg,
+                updated_at
+            });
+            return res.json({message: "Mensagem editada com sucesso!"});
+        }
+        
+        return res.status(401).json({messagem: "Operação não permitida apenas usuário que criou a mensagem ou consultor pode deletar!"})
+       
+    },
+
+
 
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tipoUsuarioController = require('./app/controllers/TipoUsuarioController');
 const UsuarioController = require('./app/controllers/UsuarioController');
+const MensagemController = require('./app/controllers/MensagemController');
+
 const routes = express.Router();
 
 // TipoUsuario
@@ -11,5 +13,10 @@ routes.post('/tipoUsuario', tipoUsuarioController.create);
 routes.get('/usuario', UsuarioController.index);
 routes.post('/usuario', UsuarioController.create);
 
+// Mensagem
+routes.get('/mensagem', MensagemController.index);
+routes.post('/mensagem', MensagemController.create);
+routes.delete('/mensagem/:id', MensagemController.delete);
+routes.put('/mensagem/:id', MensagemController.update);
 
 module.exports = routes;
